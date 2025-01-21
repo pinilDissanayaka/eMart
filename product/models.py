4be838199c0b40eac3a9a64d93d489c45162a9ea
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-01-21 21:41:01 +0530

    add base

---
+++ product/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name=models.CharField(max_length=255)
+    category_description=models.TextField()
+
+class Product(models.Model):
+    product_name=models.CharField(max_length=200)
+    product_description=models.TextField()
+    product_category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_price=models.DecimalField(max_digits=10,decimal_places=2)
+    product_stock=models.IntegerField()
+    product_image=models.ImageField(upload_to='media/',null=True,blank=True)
+    product_added=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product_name
+    
+
+
---
+++ product/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+
+def index(request):
+    top_products=Product.objects.all().order_by("-product_added")[:10]
+    return render(request, 'product/index.html', {"top_products" : top_products})
