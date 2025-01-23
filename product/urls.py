15156df85bff38a6f5b239af0c5c556f298c0106
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-01-23 11:28:20 +0530

    add statics

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", view=views.index, name="index"),
+    path("men/", view=views.men_product_view, name="men"),
 ]
--- product/views.py
+++ product/views.py
@@ -6,3 +6,6 @@ from .models import Product
 def index(request):
     top_products=Product.objects.all().order_by("-product_added")[:10]
     return render(request, 'product/index.html', {"top_products" : top_products})
+
+def men_product_view(request):
+    return render(request, 'product/men.html')
