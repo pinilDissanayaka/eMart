72f9240cf8135aff82019b174e71a90391ed7356
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-01-30 10:56:31 +0530

    add men section

--- product/models.py
+++ product/models.py
@@ -3,13 +3,22 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    category_name=models.CharField(max_length=255)
-    category_description=models.TextField()
+    CATEGORY_CHOICES=[
+        ("men","men"),
+        ("women","women")
+    ]
+
+    category_name=models.CharField(max_length=255, choices=CATEGORY_CHOICES, unique=True)
+    category_description=models.TextField(null=True,blank=True)
+
+
+    def __str__(self):
+        return self.category_name
 
 class Product(models.Model):
     product_name=models.CharField(max_length=200)
     product_description=models.TextField()
-    product_category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_category=models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     product_price=models.DecimalField(max_digits=10,decimal_places=2)
     product_stock=models.IntegerField()
     product_image=models.ImageField(upload_to='media/',null=True,blank=True)
--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Product
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from .models import Product, Category
 
 # Create your views here.
 
@@ -8,9 +9,17 @@ def index(request):
     return render(request, 'product/index.html', {"top_products" : top_products})
 
 def men_product_view(request):
-    men_products=Product.objects.filter(product_category="Men")
-    return render(request, 'product/men.html')
+    try:
+        men_products=Product.objects.filter(product_category=Category.objects.filter(category_name="men").first())
+    except Category.DoesNotExist:
+        men_products=Category.objects.none()
+    return render(request, 'product/men.html', {"products" : men_products})
 
 
 def women_product_view(request):
-    return render(request, 'product/women.html')
+    try:
+        women_products=Category.objects.get(category_name="women").products.all()
+    except Category.DoesNotExist:
+        women_products=Category.objects.none()
+
+    return render(request, 'product/women.html', {"products" : women_products})
