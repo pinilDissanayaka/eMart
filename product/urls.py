26fb931b54404b2f8d9fe6410936fb2e74c4dea8
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-02-14 07:34:11 +0530

    add hyperlink for kinds section

--- product/urls.py
+++ product/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", view=views.index, name="index"),
     path("men/", view=views.men_product_view, name="men"),
     path("women/", view=views.women_product_view, name="women"),
+    path("kids/", view=views.kids_product_view, name="kids"),
 ]
